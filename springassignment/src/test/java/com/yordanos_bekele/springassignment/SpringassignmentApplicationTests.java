81f93a9b312dc7ac313040b203a50c939ad3315d
yordanos-bekele <yordanosbeke@gmail.com>
2025-10-29 14:37:32 +0300

    Initial commit

---
+++ springassignment/src/test/java/com/yordanos_bekele/springassignment/SpringassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yordanos_bekele.springassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
